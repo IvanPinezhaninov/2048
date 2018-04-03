18f5e0e2f545a3163bf8b2635b5b78e623d10c6b
Ivan Pinezhaninov <ivan.pinezhaninov@gmail.com>
2018-04-03 21:08:34 +0300

    Build was fixed for Qt5 version less than 5.6.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QGuiApplication app(argc, argv);
     app.setOrganizationName(QLatin1Literal(ORGANIZATION_NAME));
